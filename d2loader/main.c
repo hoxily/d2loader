f024137e03c2d76e9b8a03d17666ee522dcf9888
杨敏 <hoxily@qq.com>
2020-11-13 00:33:34 +0800

    WinMain入口

--- d2loader/main.c
+++ d2loader/main.c
@@ -1,1 +1,10 @@
 #include <Windows.h>
+
+int WINAPI WinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPSTR lpCmdLine,
+    _In_ int nShowCmd)
+{
+    return 0;
+}
