716767ca69ee96963b137b70716392dd3a310b5f
杨敏 <hoxily@qq.com>
2020-11-13 02:25:02 +0800

    跟踪到一个利用CreateFileA函数来实现判断文件是否存在的函数。

--- d2loader/main.c
+++ d2loader/main.c
@@ -1,4 +1,80 @@
 #include <Windows.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+/* global variable */
+void* Dst;
+FILE* logFile;
+
+/* string constant */
+#define D2_EXP_DOT_MPQ "d2exp.mpq"
+#define D2_X_TALK_DOT_MPQ "d2xtalk.mpq"
+
+/* function prototype */
+void LogFormat(const char* format, ...);
+
+/**
+ * sub_407380
+ */
+BOOL CheckFileExist(const char* filePath)
+{
+    HANDLE file = CreateFileA(
+        filePath,
+        GENERIC_READ,
+        FILE_SHARE_READ,
+        NULL,
+        OPEN_EXISTING,
+        0,
+        NULL);
+    BOOL exist = (file != INVALID_HANDLE_VALUE);
+    CloseHandle(file);
+    return exist;
+}
+
+void sub_40735e()
+{
+    if (!CheckFileExist(D2_EXP_DOT_MPQ))
+    {
+        LogFormat("%s not exist.\n", D2_EXP_DOT_MPQ);
+        return;
+    }
+    if (!CheckFileExist(D2_X_TALK_DOT_MPQ))
+    {
+        LogFormat("%s not exist.\n", D2_X_TALK_DOT_MPQ);
+        return;
+    }
+}
+
+void sub_4069d8()
+{
+    memset(Dst, 0, 0xc94);
+    sub_40735e();
+    //TODO
+}
+
+void* sub_406803()
+{
+    Dst = malloc(0xc94);
+    if (!Dst)
+    {
+        return NULL;
+    }
+    sub_4069d8();
+    //TODO
+}
+
+void InitLogFile()
+{
+    fopen_s(&logFile, "d2loader.log", "a");
+}
+
+void LogFormat(const char* format, ...)
+{
+    va_list ap;
+    va_start(ap, format);
+    vfprintf(logFile, format, ap);
+    va_end(ap);
+}
 
 int WINAPI WinMain(
     _In_ HINSTANCE hInstance,
@@ -6,5 +82,12 @@ int WINAPI WinMain(
     _In_ LPSTR lpCmdLine,
     _In_ int nShowCmd)
 {
+    InitLogFile();
+
+    Dst = sub_406803();
+    if (!Dst)
+    {
+        return 0;
+    }
     return 0;
 }
