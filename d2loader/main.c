319b87fad7bffcc39ed6c76180c804d46451881f
杨敏 <hoxily@qq.com>
2021-06-09 16:59:54 +0800

    fix: 完成 sub_407bb9_InitializeProcessPriority 函数。

--- d2loader/main.c
+++ d2loader/main.c
@@ -221,7 +221,51 @@ BOOL sub_406bb9()
 
 BOOL sub_407bb9_InitializeProcessPriority(HANDLE hProcess, const char* priority)
 {
-    //TODO
+    // 汇编里出现 esp + 8 + hProcess, esp + 12 + hProcess, esp + 8 + priority，
+    // 是因为这个函数不是bp-based栈帧，每次push一个东西，就会导致索引变化，相比bp-based要复杂一些。
+    // IDA竟然分析出来了！
+    
+    if (!priority)
+    {
+        return FALSE;
+    }
+
+    if (!hProcess)
+    {
+        return FALSE;
+    }
+
+    DWORD priorityClass;
+    if (_stricmp(priority, CSTR_PROCESS_PRIORITY_IDLE) == 0)
+    {
+        priorityClass = IDLE_PRIORITY_CLASS;
+    }
+    else if (_stricmp(priority, CSTR_PROCESS_PRIORITY_NORMAL) == 0)
+    {
+        priorityClass = NORMAL_PRIORITY_CLASS;
+    }
+    else if (_stricmp(priority, CSTR_PROCESS_PRIORITY_BELOW_NORMAL) == 0)
+    {
+        priorityClass = BELOW_NORMAL_PRIORITY_CLASS;
+    }
+    else if (_stricmp(priority, CSTR_PROCESS_PRIORITY_ABOVE_NORMAL) == 0)
+    {
+        priorityClass = ABOVE_NORMAL_PRIORITY_CLASS;
+    }
+    else if (_stricmp(priority, CSTR_PROCESS_PRIORITY_HIGH) == 0)
+    {
+        priorityClass = HIGH_PRIORITY_CLASS;
+    }
+    else if (_stricmp(priority, CSTR_PROCESS_PRIORITY_REALTIME) == 0)
+    {
+        priorityClass = REALTIME_PRIORITY_CLASS;
+    }
+    else
+    {
+        return FALSE;
+    }
+    
+    return SetPriorityClass(hProcess, priorityClass);
 }
 
 BOOL sub_406803()
