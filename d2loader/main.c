1c6ce6b99852ecf1d0978a885a8171bcf9ce7da2
杨敏 <hoxily@qq.com>
2021-06-11 17:48:59 +0800

    完成命令行参数名搜索表。

--- d2loader/main.c
+++ d2loader/main.c
@@ -23,12 +23,120 @@ FILE* global_dd_40858c_logFile;
 
 DWORD global_dd_408590_logFlag;
 
-//TODO 需要填充这个indexTable
-DWORD global_dd_402eb8_indexTable[461];
+// 该命令行参数无具体参数值
+#define ARG_TYPE_NO_PARAM 1
+#define ARG_TYPE_04 4
 
-void* global_dd_402eac = 0x004035f0;
+#pragma pack(1)
+struct string_index_item
+{
+    // 参数的短名
+    const char* shortName;
+    // 参数分类
+    const char* category;
+    // 参数的长名
+    const char* longName;
+    // 参数的长名的长度
+    DWORD length;
+    // 参数的类型
+    DWORD type;
+};
 
-void* global_dd_402eb0 = 0x004035f8;
+struct string_index_item global_dd_402ea8_indexTable[93] = {
+    { "exp", "MAIN", "expansion", 0x0, ARG_TYPE_NO_PARAM },
+    { "w", "VIDEO", "window", 0x4, ARG_TYPE_NO_PARAM },
+    { "glide", "VIDEO", "glide", 0x6, ARG_TYPE_NO_PARAM },
+    { "opengl", "VIDEO", "opengl", 0x7, ARG_TYPE_NO_PARAM },
+    { "d3d", "VIDEO", "d3d", 0x9, ARG_TYPE_NO_PARAM },
+    { "rave", "VIDEO", "rave", 0x8, ARG_TYPE_NO_PARAM },
+    { "per", "VIDEO", "perspective", 0x9, ARG_TYPE_NO_PARAM },
+    { "lq", "VIDEO", "lowquality", 0xa, ARG_TYPE_NO_PARAM },
+    { "gamma", "VIDEO", "gamma", 0xc, 0x4 },
+    { "vsync", "VIDEO", "vsync", 0x10, ARG_TYPE_NO_PARAM },
+    { "fr", "VIDEO", "framerate", 0x10, 0x4 },
+    { "s", "NETWORK", "serverip", 0x33, 0x18 },
+    { "gametype", "NETWORK", "gametype", 0x14, 0x4 },
+    { "joinid", "NETWORK", "joinid", 0x18, 0x2 },
+    { "gamename", "NETWORK", "gamename", 0x1a, 0x18 },
+    { "bn", "NETWORK", "battlenetip", 0x4b, 0x18 },
+    { "mcpip", "NETWORK", "mcpip", 0x63, 0x18 },
+    { "nopk", "NETWORK", "nopk", 0x7e, ARG_TYPE_NO_PARAM },
+    { "openc", "NETWORK", "openc", 0x7f, ARG_TYPE_NO_PARAM },
+    { "arena", "GAME", "arena", 0x202, 0x2 },
+    { "difficulty", "GAME", "difficulty", 0x20b, ARG_TYPE_NO_PARAM },
+    { "txt", "GAME", "text", 0x211, ARG_TYPE_NO_PARAM },
+    { "ama", "CHARACTER", "ama", 0x81, ARG_TYPE_NO_PARAM },
+    { "pal", "CHARACTER", "pal", 0x81, ARG_TYPE_NO_PARAM },
+    { "sor", "CHARACTER", "sor", 0x82, ARG_TYPE_NO_PARAM },
+    { "nec", "CHARACTER", "nec", 0x83, ARG_TYPE_NO_PARAM },
+    { "bar", "CHARACTER", "bar", 0x84, ARG_TYPE_NO_PARAM },
+    { "dru", "CHARACTER", "dru", 0x85, ARG_TYPE_NO_PARAM },
+    { "asn", "CHARACTER", "asn", 0x86, ARG_TYPE_NO_PARAM },
+    { "i", "CHARACTER", "invincible", 0x87, ARG_TYPE_NO_PARAM },
+    { "bnacct", "CHARACTER", "bnacct", 0x88, 0x18 },
+    { "bnpass", "CHARACTER", "bnpass", 0xa0, 0x18 },
+    { "name", "CHARACTER", "name", 0xb9, 0x18 },
+    { "realm", "CHARACTER", "realm", 0xd1, 0x18 },
+    { "ctemp", "CHARACTER", "ctemp", 0x1e9, ARG_TYPE_NO_PARAM },
+    { "charclass", "CHARACTER", "charclass", 0x1e9, ARG_TYPE_NO_PARAM },
+    { "charstatus", "CHARACTER", "charstatus", 0x1ea, 0x2 },
+    { "nm", "MONSTER", "nomonster", 0x1ec, ARG_TYPE_NO_PARAM },
+    { "m", "MONSTER", "monsterclass", 0x1ed, 0x4 },
+    { "minfo", "MONSTER", "monsterinfo", 0x1f1, ARG_TYPE_NO_PARAM },
+    { "md", "MONSTER", "monsterdebug", 0x1f2, 0x4 },
+    { "rare", "ITEM", "rare", 0x1f6, ARG_TYPE_NO_PARAM },
+    { "unique", "ITEM", "unique", 0x1f7, ARG_TYPE_NO_PARAM },
+    { "act", "INTERFACE", "act", 0x1fb, 0x4 },
+    { "log", "DEBUGS", "log", 0x212, ARG_TYPE_NO_PARAM },
+    { "msglog", "DEBUGS", "msglog", 0x212, ARG_TYPE_NO_PARAM },
+    { "safe", "DEBUGS", "safemode", 0x213, ARG_TYPE_NO_PARAM },
+    { "nosave", "DEBUGS", "nosave", 0x215, ARG_TYPE_NO_PARAM },
+    { "seed", "DEBUGS", "seed", 0x216, 0x4 },
+    { "teen", "DEBUGS", "teen", 0x21a, ARG_TYPE_NO_PARAM },
+    { "cheats", "DEBUGS", "cheats", 0x219, ARG_TYPE_NO_PARAM },
+    { "ns", "DEBUGS", "nosound", 0x21b, ARG_TYPE_NO_PARAM },
+    { "questall", "DEBUGS", "questall", 0x21c, ARG_TYPE_NO_PARAM },
+    { "build", "DEBUGS", "build", 0x21e, ARG_TYPE_NO_PARAM },
+    { "npl", "FILEIO", "nopreload", 0x1fe, ARG_TYPE_NO_PARAM },
+    { "direct", "FILEIO", "direct", 0x1ff, ARG_TYPE_NO_PARAM },
+    { "lem", "FILEIO", "lowend", 0x200, ARG_TYPE_NO_PARAM },
+    { "nocompress", "FILEIO", "nocompress", 0x202, ARG_TYPE_NO_PARAM },
+    { "comint", "BNET", "comint", 0x221, 0x4 },
+    { "token", "BNET", "token", 0x223, 0x4 },
+    { "gamepass", "BNET", "gamepass", 0x23f, 0x18 },
+    { "skiptobnet", "BNET", "skiptobnet", 0x359, ARG_TYPE_NO_PARAM },
+    { "client", "CUSTOM", "client", 0x79b, ARG_TYPE_NO_PARAM },
+    { "server", "CUSTOM", "server", 0x79c, ARG_TYPE_NO_PARAM },
+    { "launch", "CUSTOM", "launch", 0x79d, ARG_TYPE_NO_PARAM },
+    { "multi", "CUSTOM", "multi", 0x79e, ARG_TYPE_NO_PARAM },
+    { "notitle", "CUSTOM", "notitle", 0x79f, ARG_TYPE_NO_PARAM },
+    { "res800", "CUSTOM", "res800", 0x7a0, ARG_TYPE_NO_PARAM },
+    { "res640", "CUSTOM", "res640", 0x7a1, ARG_TYPE_NO_PARAM },
+    { "nonotify", "CUSTOM", "nonotify", 0x7a2, ARG_TYPE_NO_PARAM },
+    { "noexit", "CUSTOM", "noexit", 0x7a3, ARG_TYPE_NO_PARAM },
+    { "autorest", "CUSTOM", "autorestart", 0x7a4, ARG_TYPE_NO_PARAM },
+    { "nohook", "CUSTOM", "nohook", 0x7a5, ARG_TYPE_NO_PARAM },
+    { "nochar", "CUSTOM", "nochar", 0x7a6, ARG_TYPE_NO_PARAM },
+    { "clientexit", "CUSTOM", "clientexit", 0x7a7, ARG_TYPE_NO_PARAM },
+    { "multiclient", "CUSTOM", "multiclient", 0x7a8, ARG_TYPE_NO_PARAM },
+    { "printarg", "CUSTOM", "printarg", 0x7a9, ARG_TYPE_NO_PARAM },
+    { "noscript", "CUSTOM", "noscript", 0x7aa, ARG_TYPE_NO_PARAM },
+    { "noplugin", "CUSTOM", "noplugin", 0x7ab, ARG_TYPE_NO_PARAM },
+    { "l", "CUSTOM", "debuglog", 0x7ac, ARG_TYPE_NO_PARAM },
+    { "console", "CUSTOM", "console", 0x7ad, ARG_TYPE_NO_PARAM },
+    { "rmconffile", "CUSTOM", "rmconffile", 0x7ae, ARG_TYPE_NO_PARAM },
+    { "nocleanup", "CUSTOM", "nocleanup", 0x7af, ARG_TYPE_NO_PARAM },
+    { "servername", "CUSTOM", "servername", 0x7ec, 0x18 },
+    { "pri", "CUSTOM", "priority", 0x7bc, 0x18 },
+    { "title", "CUSTOM", "title", 0x804, 0x80 },
+    { "locale", "CUSTOM", "locale", 0x7d4, 0x18 },
+    { "cfg", "CUSTOM", "conffile", 0x884, 0x104 },
+    { "mpq", "CUSTOM", "mpqfile", 0x988, 0x104 },
+    { "pdir", "CUSTOM", "plugindir", 0xa8c, 0x104 },
+    { "sfile", "CUSTOM", "scriptfile", 0xb90, 0x104 },
+    { "test", "CUSTOM", "test", 0x7b8, 0x4 },
+    { NULL, NULL, NULL, 0, 0 },
+};
 
 /* function prototype */
 
@@ -312,30 +420,30 @@ int sub_406ac0(const char* buffer)
     }
 
     int ebp_index = 0;
-    if (global_dd_402eac == ebp_index)
+    if (global_dd_402ea8_indexTable[0].category == (void*)ebp_index)
     {
         return -1;
     }
 
     // 指向去除“-”的参数名
     const char* ebx_argName = buffer + 1;
-    // 这里的字符串查找表需要重新换算。
-    char** esi_stringTableEntry = &global_dd_402eb0;
+
+    struct string_index_item* esi_stringTableEntry = &global_dd_402ea8_indexTable[0]; //&global_dd_402eb0;
     do
     {
-        const char* entry_minus8 = esi_stringTableEntry[-2];
+        const char* entry_minus8 = esi_stringTableEntry[0].shortName;
         if (lstrcmpiA(entry_minus8, ebx_argName) == 0)
         {
             break;
         }
-        const char* entry = esi_stringTableEntry[0];
+        const char* entry = esi_stringTableEntry[0].longName;
         if (lstrcmpiA(entry, ebx_argName) == 0)
         {
             break;
         }
-        esi_stringTableEntry += 5;
+        esi_stringTableEntry++;
         ebp_index++;
-    } while (esi_stringTableEntry[-1] != NULL);
+    } while (esi_stringTableEntry[0].category != NULL);
 
     return ebp_index;
 }
@@ -358,7 +466,7 @@ BOOL sub_406887(char* commandLine)
         // 这样的组合表示检测esi是否为负数。
         if (i >= 0)
         {
-            if (global_dd_402eb8_indexTable[i * 5] != 1)
+            if (global_dd_402ea8_indexTable[i].type != ARG_TYPE_NO_PARAM)
             {
                 edi_s = sub_406a68_CutFirstArgument(edi_s, buffer);
             }
