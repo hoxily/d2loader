dbbf476a4099a54e8296f3a0dd4e16e8c3474858
杨敏 <hoxily@qq.com>
2021-06-14 22:04:27 +0800

    完成 sub_406bb9_LoadVideoConfigFromRegistry

--- d2loader/main.c
+++ d2loader/main.c
@@ -602,9 +602,60 @@ BOOL sub_4068f2_LoadConfFile(const char* filename)
     return TRUE;
 }
 
-BOOL sub_406bb9()
+BOOL sub_406bb9_LoadVideoConfigFromRegistry()
 {
-    //TODO
+    DWORD data;
+    HKEY phkResult;
+    DWORD cbData;
+    DWORD type;
+
+    LSTATUS ret = RegOpenKeyExA(
+        HKEY_CURRENT_USER,
+        "Software\\Blizzard Entertainment\\Diablo II\\VideoConfig",
+        0,
+        KEY_READ,
+        &phkResult);
+    
+    if (ret != ERROR_SUCCESS)
+    {
+        return FALSE;
+    }
+
+    // Render 是一个REG_DWORD类型的数据。可以在成功执行 D2VidTst.exe 之后，使用windows的注册表编辑器看到。
+    cbData = sizeof(data);
+    ret = RegQueryValueExA(
+        phkResult,
+        "Render",
+        NULL,
+        &type,
+        &data,
+        &cbData);
+    if (ret != ERROR_SUCCESS)
+    {
+        RegCloseKey(phkResult);
+        return FALSE;
+    }
+    else
+    {
+        if (data == 1)
+        {
+            global_dd_408620_settings->padding[0x0008] = TRUE;
+        }
+        else if (data == 2)
+        {
+            global_dd_408620_settings->padding[0x0006] = TRUE;
+        }
+        else if (data == 3)
+        {
+            global_dd_408620_settings->padding[0x0005] = TRUE;
+        }
+        else if (data == 4)
+        {
+            global_dd_408620_settings->padding[0x0004] = TRUE;
+        }
+    }
+    RegCloseKey(phkResult);
+    return TRUE;
 }
 
 BOOL sub_407bb9_InitializeProcessPriority(HANDLE hProcess, const char* priority)
@@ -679,7 +730,7 @@ BOOL sub_406803_InitializeSettings()
         dstStr[3] == 0 &&
         dstStr[4] == 0)
     {
-        sub_406bb9();
+        sub_406bb9_LoadVideoConfigFromRegistry();
     }
 
     char* priority = global_dd_408620_settings->db_07bc_processPriority.value;
