4c9bc91f2e2b5428ef63670a55370f4d9e7c2650
杨敏 <hoxily@qq.com>
2021-06-26 12:52:27 +0800

    fix: 求的是当前模块的文件路径。Kernel32这个系统文件没啥好求的。 完成 sub_4054fd_HookDll 函数。

--- d2loader/main.c
+++ d2loader/main.c
@@ -336,8 +336,14 @@ typedef HWND (WINAPI *fn_FindWindowA)(
     );
 fn_FindWindowA global_dd_4085a8_FindWindowA;
 
-const char* global_dd_408598_moduleKernel32FileName;
+const char* global_dd_408598_moduleD2LoaderFileName;
 const char* global_dd_40859c_gameDotExeFileName;
+HMODULE global_dd_4085b0_moduleBnClient;
+HMODULE global_dd_4085ac_moduleD2Gfx;
+HMODULE global_dd_4085b4_moduleD2Net;
+
+char* global_dd_4023f0 = "LoadLibraryA";
+char* global_dd_402408 = "FindWindowA";
 
 void sub_404ed0_LogFormat(const char* tag, const char* format, ...)
 {
@@ -1309,25 +1315,64 @@ BOOL sub_406014_PluginInit()
     }
 }
 
+BOOL sub_40513a(
+    HMODULE hModule,
+    const char* hookDll,
+    char** functionName,
+    void* null1,
+    void* null2)
+{
+
+}
+
 BOOL sub_4054fd_HookDll()
 {
     HMODULE hModuleKernel32 = GetModuleHandleA("Kernel32.dll");
     assert(hModuleKernel32 != NULL);
-    global_dd_4085a0_LoadLibraryA = GetProcAddress(hModuleKernel32, "LoadLibraryA");
-    global_dd_4085a4_GetModuleFileNameA = GetProcAddress(hModuleKernel32, "GetModuleFileNameA");
+    global_dd_4085a0_LoadLibraryA = (fn_LoadLibraryA)GetProcAddress(hModuleKernel32, "LoadLibraryA");
+    global_dd_4085a4_GetModuleFileNameA = (fn_GetModuleFileNameA)GetProcAddress(hModuleKernel32, "GetModuleFileNameA");
     
     HMODULE hModuleUser32 = GetModuleHandleA("User32.dll");
     assert(hModuleUser32 != NULL);
-    global_dd_4085a8_FindWindowA = GetProcAddress(hModuleUser32, "FindWindowA");
+    global_dd_4085a8_FindWindowA = (fn_FindWindowA)GetProcAddress(hModuleUser32, "FindWindowA");
     
     char filename[0x104];
-    GetModuleFileNameA(hModuleKernel32, filename, sizeof(filename));
-    global_dd_408598_moduleKernel32FileName = _strdup(filename);
+    GetModuleFileNameA(NULL, filename, sizeof(filename));
+    global_dd_408598_moduleD2LoaderFileName = _strdup(filename);
 
     char* filePart;
     GetFullPathNameA("game.exe", sizeof(filename), filename, &filePart);
     global_dd_40859c_gameDotExeFileName = _strdup(filename);
-    //TODO
+
+    /*
+    * 这儿有段代码比较难理解。
+    * mov [esp + 118h + lpLibFileName], offset LibFileName ; "bnclient.dll"
+    * 
+    * 其中的 lpLibFileName 其实就是 -118h，相当于把当前栈顶元素赋予这个字符串常量首地址。
+    * 
+    * 最神奇的是在这之前调用了 __cdecl 规范的 _strdup 函数，还没有平衡栈。
+    * 也就相当于 pop ecx; mov ecx, offset LibFileName; push ecx;
+    */
+    global_dd_4085b0_moduleBnClient = LoadLibraryA("bnclient.dll");
+    global_dd_4085ac_moduleD2Gfx = LoadLibraryA("d2gfx.dll");
+    global_dd_4085b4_moduleD2Net = LoadLibraryA("d2net.dll");
+
+    BOOL ret1 = sub_40513a(
+        global_dd_4085b0_moduleBnClient,
+        "Kernel32.dll",
+        &global_dd_4023f0,
+        NULL,
+        NULL
+    );
+    BOOL ret2 = sub_40513a(
+        global_dd_4085ac_moduleD2Gfx,
+        "User32.dll",
+        &global_dd_402408,
+        NULL,
+        NULL
+    );
+    
+    return ret1 && ret2;
 }
 
 BOOL sub_40a480()
