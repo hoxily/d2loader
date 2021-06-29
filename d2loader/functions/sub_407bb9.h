abb5993094b9065a2e5260899c5d852b588b1307
杨敏 <hoxily@qq.com>
2021-06-29 14:52:52 +0800

    分离 sub_407bb9_InitializeProcessPriority

---
+++ d2loader/functions/sub_407bb9.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <Windows.h>
+
+extern BOOL sub_407bb9_InitializeProcessPriority(
+    HANDLE hProcess,
+    const char* priority
+);
--- d2loader/main.c
+++ d2loader/main.c
@@ -9,62 +9,10 @@
 #include "functions/sub_404ed0.h"
 #include "functions/sub_40735e.h"
 #include "functions/sub_406887.h"
-
-
-
-
-BOOL sub_407bb9_InitializeProcessPriority(HANDLE hProcess, const char* priority)
-{
-    // 汇编里出现 esp + 8 + hProcess, esp + 12 + hProcess, esp + 8 + priority，
-    // 是因为这个函数不是bp-based栈帧，每次push一个东西，就会导致索引变化，相比bp-based要复杂一些。
-    // IDA竟然分析出来了！
-    
-    if (!priority)
-    {
-        return FALSE;
-    }
-
-    if (!hProcess)
-    {
-        return FALSE;
-    }
-
-    DWORD priorityClass;
-    if (_stricmp(priority, CSTR_PROCESS_PRIORITY_IDLE) == 0)
-    {
-        priorityClass = IDLE_PRIORITY_CLASS;
-    }
-    else if (_stricmp(priority, CSTR_PROCESS_PRIORITY_NORMAL) == 0)
-    {
-        priorityClass = NORMAL_PRIORITY_CLASS;
-    }
-    else if (_stricmp(priority, CSTR_PROCESS_PRIORITY_BELOW_NORMAL) == 0)
-    {
-        priorityClass = BELOW_NORMAL_PRIORITY_CLASS;
-    }
-    else if (_stricmp(priority, CSTR_PROCESS_PRIORITY_ABOVE_NORMAL) == 0)
-    {
-        priorityClass = ABOVE_NORMAL_PRIORITY_CLASS;
-    }
-    else if (_stricmp(priority, CSTR_PROCESS_PRIORITY_HIGH) == 0)
-    {
-        priorityClass = HIGH_PRIORITY_CLASS;
-    }
-    else if (_stricmp(priority, CSTR_PROCESS_PRIORITY_REALTIME) == 0)
-    {
-        priorityClass = REALTIME_PRIORITY_CLASS;
-    }
-    else
-    {
-        return FALSE;
-    }
-    
-    return SetPriorityClass(hProcess, priorityClass);
-}
-
 #include "functions/sub_4069d8.h"
 #include "functions/sub_4068f2.h"
 #include "functions/sub_406bb9.h"
+#include "functions/sub_407bb9.h"
 
 BOOL sub_406803_InitializeSettings()
 {
