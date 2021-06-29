74b6d5b20d34155ff85ef5cb0d4d0a6a0dff0085
杨敏 <hoxily@qq.com>
2021-06-29 16:44:30 +0800

    分离 sub_4063f9_UnloadPlugin

---
+++ d2loader/functions/sub_4063f9.c
@@ -0,0 +1,34 @@
+#include "sub_4063f9.h"
+#include "../global-variables.h"
+#include "../constants.h"
+#include "sub_404ed0.h"
+#include "sub_406373.h"
+
+BOOL sub_4063f9_UnloadPlugin(
+    struct loaded_plugin_item* plugin
+)
+{
+    if (plugin == NULL)
+    {
+        return FALSE;
+    }
+
+    if (plugin->dd_0000_activeFlag == 0)
+    {
+        return FALSE;
+    }
+
+    sub_404ed0_LogFormat(
+        LOG_TAG(sub_4063f9_UnloadPlugin),
+        "Unloading Plugin %s",
+        plugin->dd_000c_dllFileName);
+
+    sub_406373_RunPlugin(plugin, RUN_PLUGIN_REASON_CLEANUP);
+    plugin->dd_0000_activeFlag = 0;
+    free(plugin->dd_000c_dllFileName);
+    CloseHandle(plugin->dd_0008_autoResetEventHandle);
+    FreeLibrary(plugin->dd_0004_dllModule);
+    global_dd_408618_activePluginCount--;
+
+    return TRUE;
+}
--- d2loader/main.c
+++ d2loader/main.c
@@ -11,32 +11,6 @@
 #include "functions/sub_40a4e5.h"
 #include "functions/sub_406373.h"
 
-BOOL sub_4063f9_UnloadPlugin(struct loaded_plugin_item* plugin)
-{
-    if (plugin == NULL)
-    {
-        return FALSE;
-    }
-
-    if (plugin->dd_0000_activeFlag == 0)
-    {
-        return FALSE;
-    }
-
-    sub_404ed0_LogFormat(
-        LOG_TAG(sub_4063f9_UnloadPlugin),
-        "Unloading Plugin %s",
-        plugin->dd_000c_dllFileName);
-
-    sub_406373_RunPlugin(plugin, RUN_PLUGIN_REASON_CLEANUP);
-    plugin->dd_0000_activeFlag = 0;
-    free(plugin->dd_000c_dllFileName);
-    CloseHandle(plugin->dd_0008_autoResetEventHandle);
-    FreeLibrary(plugin->dd_0004_dllModule);
-    global_dd_408618_activePluginCount--;
-
-    return TRUE;
-}
 
 int sub_4061df_PluginListRun(DWORD reasonFlag)
 {
