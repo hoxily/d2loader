900249ea01c6e86b09e0db40280b7de1074458e3
杨敏 <hoxily@qq.com>
2021-06-29 17:00:05 +0800

    分离 sub_4053b3_IsWin32NtPlatform

---
+++ d2loader/functions/sub_4053b3.c
@@ -0,0 +1,26 @@
+#include <assert.h>
+#include "sub_4053b3.h"
+
+BOOL sub_4053b3_IsWin32NtPlatform(
+)
+{
+    OSVERSIONINFOA version;
+    memset(&version, 0, sizeof(version));
+    assert(offsetof(OSVERSIONINFOA, dwOSVersionInfoSize) == 0);
+    version.dwOSVersionInfoSize = sizeof(version);
+
+    // ¡Ÿ ±Ω˚”√ obsolete°£∑Ò‘Úvisual studio±‡“Î ß∞‹°£
+#pragma warning(disable: 4996)
+    BOOL ret = GetVersionExA(&version);
+#pragma warning(default: 4996)
+
+    if (ret)
+    {
+        assert(offsetof(OSVERSIONINFOA, dwPlatformId) == 0x10);
+        if (version.dwPlatformId == VER_PLATFORM_WIN32_NT)
+        {
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
---
+++ d2loader/functions/sub_4053b3.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <Windows.h>
+
+extern BOOL sub_4053b3_IsWin32NtPlatform(
+);
