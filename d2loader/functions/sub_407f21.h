c04cf5a72e6730a112de7b55b34b34020ab1d72b
杨敏 <hoxily@qq.com>
2021-07-12 10:03:14 +0800

    补充SplitString返回的指针的结构。

--- d2loader/functions/sub_407f21.h
+++ d2loader/functions/sub_407f21.h
@@ -7,7 +7,7 @@
 * @param pattern 被切分的字符串
 * @param s 用以分隔的字符数组
 * @param count 返回切割完成的字符串数组的元素个数
-* @returns 切割后的字符串数组
+* @returns 切割后的字符串数组。字符串指针数组以及指向的字符串都已经打包到单块malloc内存里。释放时只需free该指针即可。
 */
 char** sub_407f21_SplitString(
     const char* pattern,
