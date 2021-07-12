f9e635a8a922321d7d23f0405cc0979b8e00ef49
杨敏 <hoxily@qq.com>
2021-07-12 14:32:54 +0800

    使用 struct string_array 将字符串数组相关数据集中管理。

--- d2loader/functions/sub_407f21.c
+++ d2loader/functions/sub_407f21.c
@@ -16,6 +16,29 @@ union string_array_item
     char* pointer;
 };
 
+/*
+* 将字符串数组相关的变量集中到一起管理
+*/
+struct string_array
+{
+    /*
+    * 数据存储区
+    */
+    union string_array_item* data;
+    /*
+    * 当前元素个数
+    */
+    int length;
+    /*
+    * 加上结尾结束标记的元素个数。等价于 length + 1
+    */
+    int lengthWithSentinel;
+    /*
+    * 存储区的元素容量
+    */
+    int capacity;
+};
+
 // 每次扩容时增加的元素数量
 #define STRING_ARRAY_INCREMENT 0x20
 // 初始元素数量
@@ -47,15 +70,16 @@ char** sub_407f21_SplitString(
         return NULL;
     }
 
-    size_t var_10 = STRING_ARRAY_INIT_ELEMENT_SIZE;
-    union string_array_item* ebx_stringArray = (union string_array_item*)malloc(sizeof(char*) * STRING_ARRAY_INIT_ELEMENT_SIZE);
-    if (ebx_stringArray == NULL)
+    int var_10 = STRING_ARRAY_INIT_ELEMENT_SIZE;
+    struct string_array ebx_stringArray;
+    ebx_stringArray.data = (union string_array_item*)malloc(sizeof(char*) * STRING_ARRAY_INIT_ELEMENT_SIZE);
+    if (ebx_stringArray.data == NULL)
     {
         free(buffer);
         return NULL;
     }
-    int stringArrayLength = 0;
-    ebx_stringArray[0].offset = 0;
+    ebx_stringArray.length = 0;
+    ebx_stringArray.data[0].offset = 0;
     char* esi_ptr = buffer;
     BOOL var_8 = TRUE;
     const char* edi_ptr = pattern;
@@ -63,9 +87,9 @@ char** sub_407f21_SplitString(
     {
         goto constructResult;
     }
-    size_t stringArrayCapacity = STRING_ARRAY_INIT_ELEMENT_SIZE;
+    ebx_stringArray.capacity = STRING_ARRAY_INIT_ELEMENT_SIZE;
     // 包括NULL结束标识的情况下，字符串数组的长度。
-    unsigned int stringArrayLengthWithSentinel = 1;
+    ebx_stringArray.lengthWithSentinel = 1;
     for (; *edi_ptr != '\0'; edi_ptr++)
     {
         // 这里并不是想在pattern中搜索s，而是s中的任何单个字符都是分割符。
@@ -77,8 +101,8 @@ char** sub_407f21_SplitString(
                 {
                     *esi_ptr = '\0';
                     esi_ptr++;
-                    stringArrayLength++;
-                    stringArrayLengthWithSentinel++;
+                    ebx_stringArray.length++;
+                    ebx_stringArray.lengthWithSentinel++;
                     var_8 = TRUE;
                 }
                 goto breakToOutterLoopEnd;;
@@ -87,22 +111,25 @@ char** sub_407f21_SplitString(
 
         if (var_8)
         {
-            if (stringArrayLengthWithSentinel >= var_10)
+            if (ebx_stringArray.lengthWithSentinel >= var_10)
             {
                 var_10 += STRING_ARRAY_INCREMENT;
-                stringArrayCapacity += STRING_ARRAY_INCREMENT;
-                union string_array_item* tmp = (union string_array_item*)realloc(ebx_stringArray, stringArrayCapacity * sizeof(char*));
+                ebx_stringArray.capacity += STRING_ARRAY_INCREMENT;
+                union string_array_item* tmp = (union string_array_item*)realloc(
+                    ebx_stringArray.data,
+                    ebx_stringArray.capacity * sizeof(char*)
+                );
                 if (tmp == NULL)
                 {
-                    free(ebx_stringArray);
+                    free(ebx_stringArray.data);
                     free(buffer);
                     return NULL;
                 }
-                ebx_stringArray = tmp;
+                ebx_stringArray.data = tmp;
             }
 
             var_8 = FALSE;
-            ebx_stringArray[stringArrayLength].offset = esi_ptr - buffer;
+            ebx_stringArray.data[ebx_stringArray.length].offset = esi_ptr - buffer;
         }
 
         *esi_ptr = *edi_ptr;
@@ -116,7 +143,7 @@ char** sub_407f21_SplitString(
     {
         *esi_ptr = '\0';
         esi_ptr++;
-        stringArrayLength++;
+        ebx_stringArray.length++;
     }
 
 constructResult:
@@ -126,13 +153,13 @@ constructResult:
     * 2. 数组结束标识 NULL；
     * 3. char* 数组针指向的字符串数据；
     */
-    size_t pointerBlockSize = sizeof(char*) * (stringArrayLength + 1);
+    size_t pointerBlockSize = sizeof(char*) * (ebx_stringArray.length + 1);
     size_t stringDataBlockSize = esi_ptr - buffer;
     char* resultBuffer = (char*)malloc(pointerBlockSize + stringDataBlockSize);
     if (resultBuffer == NULL)
     {
         free(buffer);
-        free(ebx_stringArray);
+        free(ebx_stringArray.data);
         return NULL;
     }
 
@@ -142,23 +169,23 @@ constructResult:
         stringDataBlockSize
     );
 
-    for (int i = 0; i < stringArrayLength; i++)
+    for (int i = 0; i < ebx_stringArray.length; i++)
     {
-        ebx_stringArray[i].pointer = ebx_stringArray[i].offset + resultBuffer + pointerBlockSize;
+        ebx_stringArray.data[i].pointer = ebx_stringArray.data[i].offset + resultBuffer + pointerBlockSize;
     }
-    ebx_stringArray[stringArrayLength].pointer = NULL;
+    ebx_stringArray.data[ebx_stringArray.length].pointer = NULL;
 
     memcpy(
         resultBuffer,
-        ebx_stringArray,
+        ebx_stringArray.data,
         pointerBlockSize
     );
 
     free(buffer);
-    free(ebx_stringArray);
+    free(ebx_stringArray.data);
     if (count != NULL)
     {
-        *count = stringArrayLength;
+        *count = ebx_stringArray.length;
     }
     return (char**)resultBuffer;
 }
