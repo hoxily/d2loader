6ed27f3509a8949d6a33edbb74c91d8f9ab3fcae
杨敏 <hoxily@qq.com>
2021-07-29 19:42:10 +0800

    complete d2lang_10000

--- D2Lang/functions/d2lang-10000.cpp
+++ D2Lang/functions/d2lang-10000.cpp
@@ -6,6 +6,24 @@ unsigned int __stdcall d2lang_10000_HashString(
     unsigned int n
 )
 {
-    //TODO
-    return 0;
+    unsigned int eax_hash = 0;
+    for (const char* edx_ptr = str; *edx_ptr != '\0'; edx_ptr++)
+    {
+        // movsx ecx, cl 表示保留符号位做扩展。
+        // 不加 (int) 也是movsx。
+        // (unsigned char) 才会变成 movzx
+        unsigned int ecx = (int)*edx_ptr;
+        eax_hash <<= 4;
+        eax_hash += ecx;
+        ecx = eax_hash;
+        ecx &= 0xF0000000;
+        if (ecx != 0)
+        {
+            ecx >>= 0x18;
+            ecx ^= eax_hash;
+            ecx &= 0x0FFFFFFF;
+            eax_hash = ecx;
+        }
+    }
+    return eax_hash % n;
 }
